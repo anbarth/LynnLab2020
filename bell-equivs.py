24ad6c4bbec1617c70041b0948f6b7ebec660d1f
Cathy <cathy.chang.ct@gmail.com>
2019-10-11 15:50:21 -0700

    first version
    first version of fillBucket, sortSubsets

--- bell-equivs.py
+++ bell-equivs.py
@@ -111,3 +111,35 @@ def hasSevenOnes(str):
         return True
     else:
         return False
+
+#initialize 27 empty buckets in a bucket list
+BucketList = [[], [], [], [], [], [], [], [], [],
+                [], [], [], [], [], [], [], [], [],
+                [], [], [], [], [], [], [], [], []]
+
+#initialize currentBucket to be -1, so the bucket starts at bucketList[0]
+
+# Main function to fill up all 27 buckets
+def sortSubsets():
+    if usedList(i) == 1: continue
+    else:
+        #retrieve the next bucket
+        newBucket=[]
+        fillBucket(newBucket, i)
+        bucketList.append(newBucket)
+
+
+
+# When first called in the main function, fill up an empty bucket. When called recursively, fill up same bucket.
+def fillBucket(bucket, i):
+    # go through the generators for each state
+    for gen in gens:
+        newSubset = indexToSubset(i) * gen
+        newIndex = subsetToIndex(newSubset)
+        # if newIndex hasn't been marked, then add to bucket the same bucket and recurse.
+        if usedList(newIndex)==0:
+            bucket.append(newIndex)
+            fillBucket(bucket, newIndex)
+        # if the newIndex has been marked, then update the bucketList
+        else:
+            BucketList[currentBucketIndex].extend(bucket)
