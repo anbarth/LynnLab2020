7e3e5dfaa95cde8c045105df415418651dd47ee9
anbarth <anna2nsb@gmail.com>
2019-10-11 16:42:34 -0700

    anna wrote indexToSubset and subsetToIndex, cathy wrote the fillBucket helper and the biiiig loop

--- bell-equivs.py
+++ bell-equivs.py
@@ -1,4 +1,5 @@
 import numpy
+from scipy.special import comb
 
 ## all of these are the TRANSPOSES of what they should be
 ic = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
@@ -73,18 +74,78 @@ sp = [[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 
 gens = [ic, ip, sc, sp]
 
-#for num in range(2^16):
-#    str = "{0:b}".format(num)
-#    if not hasSevenOnes(str):
-#        continue
+
+#initialize 27 empty buckets in a bucket list
+BucketList = [[], [], [], [], [], [], [], [], [],
+                [], [], [], [], [], [], [], [], [],
+                [], [], [], [], [], [], [], [], []]
+
+#initialize currentBucket to be -1, so the bucket starts at bucketList[0]
+
+# Main function to fill up all 27 buckets
+def sortSubsets():
+    if usedList(i) == 1: continue
+    else:
+        #retrieve the next bucket
+        newBucket=[]
+        fillBucket(newBucket, i)
+        bucketList.append(newBucket)
+
+
+
+# When first called in the main function, fill up an empty bucket. When called recursively, fill up same bucket.
+def fillBucket(bucket, i):
+    # go through the generators for each state
+    for gen in gens:
+        newSubset = indexToSubset(i) * gen
+        newIndex = subsetToIndex(newSubset)
+        # if newIndex hasn't been marked, then add to bucket the same bucket and recurse.
+        if usedList(newIndex)==0:
+            bucket.append(newIndex)
+            fillBucket(bucket, newIndex)
+        # if the newIndex has been marked, then update the bucketList
+        else:
+            BucketList[currentBucketIndex].extend(bucket)
+
+
+
+# given a subset's index (0 to 11440-1),
+# return the subset of 7 bell states
+def indexToSubset(i):
+    subset = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
     
-#    for gen in gens:
+    n = 15
+    k = 6
+
+    for pos in range(len(subset)):
+        nChooseK = comb(n, k,exact=True) # avoid doing this computation twice
+        if i < nChooseK:
+            subset[pos] = 1
+            k = k-1
+        else:
+            i = i-nChooseK
+        n = n-1
+
+    return subset
 
 
+# given a subset of 7 bell states,
+# return the corresponding index from 0 to 11440-1
+def subsetToIndex(subset):
+    i = 0
 
+    n = 15
+    k = 6
 
-
-
+    for pos in range(len(subset)):
+        if subset[pos] == 0:
+            i = i + comb(n,k,exact=True)
+        else:
+            k = k-1
+        n = n-1
+    
+    return i
+            
 
 # given a 16-vector representing a set of bell states,
 # print the tic-tac-toe diagram
@@ -100,14 +161,3 @@ def tictactoe(v):
                 s += " - "
         s += '\n'
     print(s)
-
-def hasSevenOnes(str):
-    numOnes = 0
-    for c in str:
-        if c == '1':
-            numOnes += 1
-            
-    if numOnes == 7:
-        return True
-    else:
-        return False
