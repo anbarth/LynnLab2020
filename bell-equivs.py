2c556678125a778df3c15330858d30e960e47b29
anbarth <anna2nsb@gmail.com>
2019-10-13 21:42:24 -0700

    now can find (and print!) representatives of all 27 classes! hooray!

--- bell-equivs.py
+++ bell-equivs.py
@@ -73,29 +73,34 @@ sp = [[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 
 gens = [ic, ip, sc, sp]
 
-usedList = [0] * 11440
 
-#initialize 27 empty buckets in a bucket list
-BucketList = [[], [], [], [], [], [], [], [], [],
-                [], [], [], [], [], [], [], [], [],
-                [], [], [], [], [], [], [], [], []]
 
-#initialize currentBucket to be -1, so the bucket starts at bucketList[0]
 
-# Main function to fill up all 27 buckets
+# sorts sets of 7 qu4it bell states into LELM equivalence classes
+# returns a list of equivalence classes
 def sortSubsets():
-    if usedList(i) == 1: 
-        print('fix me later')
-        #continue
-    else:
-        #retrieve the next bucket
-        newBucket=[]
-        fillBucket(newBucket, [i])
-        bucketList.append(newBucket)
+    # list to keep track of which subsets have already been sorted
+    usedList = [0] * 11440
 
+    # list to be filled with equivalence class buckets
+    bucketList = []
 
+    # iterate through all possible subsets
+    for i in range(11440):
+        # subset already sorted -- move on
+        if usedList[i] == 1: 
+            continue
+        else:
+            # create and populate a new equivalence class
+            newBucket=[i]
+            fillBucket(newBucket, newBucket)
+            bucketList.append(newBucket)
+    
+    return bucketList
 
-# When first called in the main function, fill up an empty bucket. When called recursively, fill up same bucket.
+# helper for sortSubsets
+# recursively finds all subsets generated from the 
+# subsets in indicesToExplore. adds these subsets to bucket
 def fillBucket(bucket, indicesToExplore):
     nextIndicesToExplore = []
     for index in indicesToExplore:
@@ -104,25 +109,21 @@ def fillBucket(bucket, indicesToExplore):
         for gen in gens:
             
             newSubset = numpy.matmul(gen, indexToSubset(index))
-            print(newSubset)
+
             newIndex = subsetToIndex(newSubset)
             # if newIndex hasn't been marked, then add to bucket the same bucket and plan to explore this new state
-            
             if usedList[newIndex]==0:
-                #print("aaaa")
                 bucket.append(newIndex)
                 nextIndicesToExplore.append(newIndex)
                 usedList[newIndex] = 1
-            # if the newIndex has been marked, then update the bucketList
-            #else:
-            #    BucketList[currentBucketIndex].extend(bucket)
+
     if len(nextIndicesToExplore) > 0:
         fillBucket(bucket, nextIndicesToExplore)
 
 
 
 # given a subset's index (0 to 11440-1),
-# return the subset of 7 bell states
+# return the corresponding subset of 7 bell states
 def indexToSubset(i):
     subset = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
     
@@ -157,7 +158,16 @@ def subsetToIndex(subset):
         n = n-1
     
     return i
-            
+
+
+# given a list of equivalence classes (e.g. the output of sortSubsets),
+# prints a tictactoe diagram of 1 representative from each class        
+def showRepresentatives(bucketList):
+    print("displaying representatives from "+str(len(bucketList))+" equivalence classes")
+
+    for i in range(len(bucketList)):
+        tictactoe(indexToSubset(bucketList[i][0]))
+
 
 # given a 16-vector representing a set of bell states,
 # print the tic-tac-toe diagram
